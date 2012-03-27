a9731603c9936e8a40e42f7c156c225dbc532a68
Michal Walczak <magikmw@michalwalczak.eu>
2012-03-27 09:26:47 +0200

    added a console log handler for INFO

--- main/__main__.py
+++ main/__main__.py
@@ -98,17 +98,20 @@ logg = logging.getLogger('Main')
 logg.setLevel(logging.DEBUG)
 
 # create a handler and set level
-#logg_ch = logging.StreamHandler()
-logg_ch = logging.FileHandler('warden.log', mode='a', encoding=None, delay=False)
-logg_ch.setLevel(logging.DEBUG)
+logg_ch = logging.StreamHandler()
+logg_fh = logging.FileHandler('warden.log', mode='a', encoding=None, delay=False)
+logg_ch.setLevel(logging.INFO)
+logg_fh.setLevel(logging.DEBUG)
 
 # crate a formatter and add it to the handler
 # [HH:MM:SS AM][LEVEL] Message string
-logg_formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', datefmt='%M:%S')
+logg_formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', datefmt='%I:%M:%S')
 logg_ch.setFormatter(logg_formatter)
+logg_fh.setFormatter(logg_formatter)
 
 # add ch to logger
-logg.addHandler(logg_ch)
+logg.addHandler(logg_ch) #console handler at level INFO
+logg.addHandler(logg_fh) #file handler at level DEBUG for more detail
 
 # ready to go!
 # logging convention:
